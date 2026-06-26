f7037a3f82487abd41edd8841eed888199626df0
thesuperRL <ryanli1366@gmail.com>
2026-06-26 19:56:35 -0400

    feat: covariance matrix

---
+++ lib/covariance.py
@@ -0,0 +1,28 @@
+import torch
+from process_image import tensorify, assign_coordinates
+from parameter_init import initialize_parameters
+
+def build_sigma(scale, radians):
+    # activate scales and radians
+    exp_scale = torch.exp(scale)
+    s = torch.diag_embed(exp_scale)
+
+    cos = torch.cos(radians)
+    sin = torch.sin(radians)
+    col1 = torch.stack((cos, sin), dim=1)
+    col2 = torch.stack((-sin, cos), dim=1) 
+    r = torch.stack((col1, col2), dim=2).squeeze(dim=3)
+    
+    sigma = r @ s @ s.transpose(-1, -2) @ r.transpose(-1, -2)
+    sigma_inv = torch.linalg.inv(sigma)
+
+    return sigma_inv
+
+if __name__ == "__main__":
+    image, shape = tensorify("images/shuak.png", resize = [256, 256])
+
+    coords = assign_coordinates(image)
+
+    _, scale, _, _, rot = initialize_parameters(image, coords)
+
+    print(build_sigma(scale, rot))
